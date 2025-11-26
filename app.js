74e712daac732880e2c69717aeea9f3c50e905c5
Cleve-keem <belloharyourmidey@gmail.com>
2025-11-26 17:08:39 -0600

    chore: completed number game

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import { input } from "@inquirer/prompts";
-import { log as print } from "./utils/logger.js";
+import { error, log as print, succes, warning } from "./utils/logger.js";
 import { select } from "@inquirer/prompts";
 import { setGameDifficulty } from "./helper/gameDifficulty.js";
 import { generateRandomNumber } from "./services/randomGenerator.js";
 import { verifyGuess } from "./services/gameLogic.js";
 
-log(`Welcome to the Number Guessing Game!
+print(`Welcome to the Number Guessing Game!
 I'm thinking of a number between 1 and 100.
 You have 5 chances to guess the correct number. \n`);
 
@@ -33,15 +33,41 @@ const option = {
 async function Main() {
   const choice = await select(option);
   const level = setGameDifficulty(choice);
-  log(
+  print(
     `\nGreat! You have selected the ${level.difficulty} difficulty level. \nLet's start the game!\n`
   );
 
+  let attempts = level.chances;
   const randNum = generateRandomNumber();
-  const guess = await input({ message: "Enter your guess:" });
-  const answer = verifyGuess(randNum, guess);
-  if (answer === "correct") {
-    print()
+
+  while (attempts >= 0) {
+    if (attempts === 0) {
+      print(
+        error(
+          `You ran out of chances ${attempts} attempts left, try again later.`
+        )
+      );
+      process.exit(0);
+    }
+
+    const guess = await input({ message: "Enter your guess:" });
+    const answer = verifyGuess(randNum, guess);
+
+    if (answer === "correct") {
+      print(
+        succes(
+          `Congratulations! You guessed the correct number in ${attempts} attempts`
+        )
+      );
+      process.exit(0);
+    }
+    if (answer === "high") {
+      print(warning(`Incorrect! The number is greater than ${guess}\n`));
+    } else {
+      print(warning(`Incorrect! The number is less than ${guess}\n`));
+    }
+
+    attempts--;
   }
 }
 
